510a2cdee879897a9235f015eea0e1085eaacd88
bryanuni1809 <bryanuni1809@gmail.com>
2025-10-02 18:55:08 -0500

    Arreglos

--- Semana 7/Proyecto (Java 8)/src/main/java/entidades/Ordenaciones.java
+++ Semana 7/Proyecto (Java 8)/src/main/java/entidades/Ordenaciones.java
@@ -30,21 +30,21 @@ public class Ordenaciones{
     int n = lista.size();
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
+        for (int j=i + 1; j < n; j++) {
             if (comp.compare(lista.get(j), lista.get(minIndex)) < 0) {
                 minIndex = j;
             }
         }
         T temp=lista.get(i);
         lista.set(i, lista.get(minIndex));
-        lista.set(minIndex, temp);
+        lista.set(minIndex,temp);
     }
 }
-    public static <T> void insercion(List<T> lista, Comparator<T> comp) {
+    public static <T> void insercion(List<T> lista, Comparator<T> comp){
     int n = lista.size();
     for (int i = 1; i < n; i++) {
         T key = lista.get(i);
-        int j = i - 1;
+        int j =i - 1;
         while (j >= 0 && comp.compare(lista.get(j), key) > 0) {
             lista.set(j + 1, lista.get(j));
             j--;
--- Semana 7/Proyecto (Java 8)/src/main/java/entidades/Persona.java
+++ Semana 7/Proyecto (Java 8)/src/main/java/entidades/Persona.java
@@ -17,33 +17,33 @@ public abstract class Persona {
     protected String correo;
 
     public Persona(String dni, String nombres, String apellidos, String direccion, String telefono, String correo) {
-        this.dni = dni;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.correo = correo;
+        this.dni= dni;
+        this.nombres =nombres;
+        this.apellidos=apellidos;
+        this.direccion =direccion;
+        this.telefono =telefono;
+        this.correo=correo;
     }
-    public String getDni() { return dni; }
-    public void setDni(String dni) { this.dni = dni; }
+    public String getDni(){return dni;}
+    public void setDni(String dni){this.dni= dni;}
 
-    public String getNombres() { return nombres; }
+    public String getNombres(){return nombres; }
     public void setNombres(String nombres) { this.nombres = nombres; }
 
-    public String getApellidos() { return apellidos; }
+    public String getApellidos(){return apellidos; }
     public void setApellidos(String apellidos) { this.apellidos = apellidos; }
 
-    public String getDireccion() { return direccion; }
+    public String getDireccion() {return direccion; }
     public void setDireccion(String direccion) { this.direccion = direccion; }
 
-    public String getTelefono() { return telefono; }
+    public String getTelefono() {return telefono; }
     public void setTelefono(String telefono) { this.telefono = telefono; }
 
-    public String getCorreo() { return correo; }
+    public String getCorreo() {return correo; }
     public void setCorreo(String correo) { this.correo = correo; }
 
     public String mostrarInfo() {
-        return "DNI: " + dni + " | Nombre: " + nombres + " " + apellidos;
+        return "DNI: " + dni +" | Nombre: " + nombres + " " + apellidos;
     }
     public abstract String getTipo();
 }
