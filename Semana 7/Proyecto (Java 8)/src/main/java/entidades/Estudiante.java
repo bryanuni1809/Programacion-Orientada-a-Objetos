1fbcdd4af1182ab286e003cd96d7116fddf8e4bc
bryanuni1809 <bryanuni1809@gmail.com>
2025-10-02 18:52:46 -0500

    Arreglos

--- Semana 7/Proyecto (Java 8)/src/main/java/entidades/Estudiante.java
+++ Semana 7/Proyecto (Java 8)/src/main/java/entidades/Estudiante.java
@@ -19,15 +19,15 @@ public class Estudiante extends Persona{
         this.nivelEstudios = nivelEstudios;
     }
 
-    public String getFechaNacimiento() { return fechaNacimiento; }
-    public void setFechaNacimiento(String fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+    public String getFechaNacimiento(){return fechaNacimiento;}
+    public void setFechaNacimiento(String fechaNacimiento){ this.fechaNacimiento = fechaNacimiento; }
 
-    public String getNivelEstudios() { return nivelEstudios; }
-    public void setNivelEstudios(String nivelEstudios) { this.nivelEstudios = nivelEstudios; }
+    public String getNivelEstudios(){return nivelEstudios;}
+    public void setNivelEstudios(String nivelEstudios){ this.nivelEstudios = nivelEstudios; }
 
     @Override
-    public String mostrarInfo() {
-        return super.mostrarInfo() + " | Fecha Nac: " + fechaNacimiento + " | Nivel: " + nivelEstudios;
+    public String mostrarInfo(){
+        return super.mostrarInfo()+ " | Fecha Nac: " +fechaNacimiento+" | Nivel: "+nivelEstudios;
     }
 
     @Override
--- Semana 7/Proyecto (Java 8)/src/main/java/entidades/Matricula.java
+++ Semana 7/Proyecto (Java 8)/src/main/java/entidades/Matricula.java
@@ -8,26 +8,26 @@ package entidades;
  *
  * @author BRYAN
  */
-public class Matricula {
+public class Matricula{
     private String codigoCurso;
     private String dniEstudiante;
     private String fechaMatricula;
     private double monto;
 
     public Matricula(String codigoCurso, String dniEstudiante, String fechaMatricula, double monto) {
-        this.codigoCurso = codigoCurso;
-        this.dniEstudiante = dniEstudiante;
-        this.fechaMatricula = fechaMatricula;
+        this.codigoCurso=codigoCurso;
+        this.dniEstudiante=dniEstudiante;
+        this.fechaMatricula =fechaMatricula;
         this.monto = monto;
     }
 
-    public String getCodigoCurso() { return codigoCurso; }
+    public String getCodigoCurso(){ return codigoCurso; }
     public void setCodigoCurso(String codigoCurso) { this.codigoCurso = codigoCurso; }
 
-    public String getDniEstudiante() { return dniEstudiante; }
+    public String getDniEstudiante(){ return dniEstudiante; }
     public void setDniEstudiante(String dniEstudiante) { this.dniEstudiante = dniEstudiante; }
 
-    public String getFechaMatricula() { return fechaMatricula; }
+    public String getFechaMatricula(){ return fechaMatricula; }
     public void setFechaMatricula(String fechaMatricula) { this.fechaMatricula = fechaMatricula; }
 
     public double getMonto() { return monto; }
